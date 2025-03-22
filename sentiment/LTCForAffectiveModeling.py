250804a65df32261e14b1d817ba1f56e1e7ecbf6
azzenn4 <69201270+azzenn4@users.noreply.github.com>
2025-03-23 02:14:47 +0700

    Update LTCForAffectiveModeling.py

--- sentiment/LTCForAffectiveModeling.py
+++ sentiment/LTCForAffectiveModeling.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.random as random
 import torchdiffeq
-import random
 from torch.utils.data import DataLoader, TensorDataset
 import numpy as np
 import matplotlib.pyplot as plt
